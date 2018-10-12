d8b30c2aeee0b1482e47a10fff2d7e7f5d3633c4
Cristian Tanase <cristian.tanase@emag.ro>
2018-10-12 16:56:37 +0300

    uncommenting methods

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -6,28 +6,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDb server');
 
-    //deleteMany
-    // db.collection('Todos').deleteMany({text: 'Something to test'}).then((result) => {
-    //     console.log(result);
-    // });
+    deleteMany
+    db.collection('Todos').deleteMany({text: 'Something to test'}).then((result) => {
+        console.log(result);
+    });
 
-    //deleteOne
-    // db.collection('Todos').deleteOne({text: 'Eat'}).then((result) => {
-    //     console.log(result);
-    // });
+    deleteOne
+    db.collection('Todos').deleteOne({text: 'Eat'}).then((result) => {
+        console.log(result);
+    });
 
-    //findOneAndDelete
-    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-    //     console.log(result);
-    // });
-    //
-    // db.collection('Users').deleteMany({name: 'Cristian'}).then((result) => {
-    //     console.log(result);
-    // });
+    findOneAndDelete
+    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+        console.log(result);
+    });
 
-    // db.collection('Users').findOneAndDelete({ _id: new ObjectID('5bc09f2c1712efbbe6409f9d')}).then((result) => {
-    //     console.log(result);
-    // });
+    db.collection('Users').deleteMany({name: 'Cristian'}).then((result) => {
+        console.log(result);
+    });
+
+    db.collection('Users').findOneAndDelete({ _id: new ObjectID('5bc09f2c1712efbbe6409f9d')}).then((result) => {
+        console.log(result);
+    });
 
 
 
